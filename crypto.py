7d57a8e9d32523b0019a5606f0f7450d42a07cd6
owen-eternal <poesylyrics@gmail.com>
2021-02-27 02:28:54 +0200

    recreated the geneate_keys for loop to a while loop and added the encryption and decryption  function

--- crypto.py
+++ crypto.py
@@ -1,11 +1,11 @@
 import math
 import random
 
-"""Developer: Owen O. Phakade""
-""""This is my implementation of an asymmetric crytographic algorithm called RSA. based on Modular arithmatic (Eucleadian Algorithm & Extended EA) """
-""" This module has four function: 
-        choose_e( takes totient of n as an argument ) -> randomly chooses a prime when combined with the totient the gcd is 1.
-        generate_keys() -> generates a public and private key pair. """
+"""Developer: Owen O. Phakade"""
+# """"This is my implementation of an asymmetric crytographic algorithm called RSA. based on Modular arithmatic (Eucleadian Algorithm & Extended EA) """
+# """ This module has four function:
+#         choose_e( takes totient of n as an argument ) -> randomly chooses a prime when combined with the totient the gcd is 1.
+#         generate_keys() -> generates a public and private key pair. """
 
 
 def choose_e(totient):
@@ -18,8 +18,8 @@ def choose_e(totient):
 
 def generate_keys():
     # select two prime numbers
-    p = 29
-    q = 23
+    p = 11
+    q = 17
     # compute the modulus
     n = p*q
     # compute the totient of n
@@ -27,9 +27,41 @@ def generate_keys():
     # choose e //public key
     e = choose_e(T)
     # compute the private key
+    k = 0
     d = 0
-    for k in range(1, e):
-        if ((1+k*T)/e).is_integer():
-            d = k
+    while True:
+        k = k + 1
+        d = (1+k*T)/e
+        if d.is_integer():
             break
+
     return e, d, n
+
+
+def encrypt(m, e, n):
+    # this function takes in a message
+    # from the sender and encrypts it
+    # using the public key and returns
+    # cipher text
+    return m**e % n
+
+
+def decrypt(c, d, n):
+    # This function takes in a cipher text
+    # from the sender and dencrypts it
+    # using the private key and returns
+    # the original text
+    return c**int(d) % n
+
+
+if __name__ == '__main__':
+
+    e, d, modulus = generate_keys()
+    message = 90
+    print("--------------------------------")
+    print(f'keys: e:{e}  d:{d}  n:{modulus}')
+    print("--------------------------------")
+    cipher_text = encrypt(message, e, modulus)
+    print(f'cipher text: {cipher_text}')
+    decrypted_msg = decrypt(cipher_text, d, modulus)
+    print(f'send Text: {decrypted_msg}')
